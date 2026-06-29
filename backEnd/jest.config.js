c7cbc866e24249aaf7b0b5caa4f14592c44bd194
SASdiop22 <serigneasdiop@gmail.com>
2026-06-29 14:36:41 +0200

    Add Jest test setup

---
+++ backEnd/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@use-cases/(.*)$': '<rootDir>/src/use-cases/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
+};
